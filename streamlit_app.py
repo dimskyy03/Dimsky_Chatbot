7a2a1e4770d9820d13c26bb52bc4ddd0ebddadbe
Dimas Widianto Ramadhan <65808813+dimskyy03@users.noreply.github.com>
2025-04-25 09:37:03 +0000

    Update code and API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,61 @@
 from google import generativeai as genai
 from dotenv import load_dotenv
-import os
 import streamlit as st
 
 # Load environment variables
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
+#load_dotenv()
+#API_KEY = os.getenv('API_KEY')
 
+API_KEY = st.text_input("OpenAI API Key", type="password")
 
 # Check API key
 if not API_KEY:
     raise ValueError("API key not found. Please set again your API key.")
+else:
+    # Setup Gemini Client
+    client = genai.configure(api_key= API_KEY)
 
-# Setup Gemini Client
+    # choose the model from sidebar
+    choosen_model = st.sidebar.selectbox("Choose gemini model", [model.name for model in genai.list_models()])
 
-client = genai.configure(api_key= API_KEY)
+    # Function to call Google AI API
+    def call_google_ai(prompt, model_name=choosen_model):
+        # Initialize the model
+        model = genai.GenerativeModel(model_name)  # Use a valid model name
+        # Generate content
+        response = model.generate_content(prompt)
+        # Strip leading newline if present
+        if response.text.startswith('\n'):
+            response.text = response.text[1:]
+        return response.text
 
-# choose the model from sidebar
-choosen_model = st.sidebar.selectbox("Choose gemini model", [model.name for model in genai.list_models()])
+    # streamlit app
+    st.title("💬Google Gemini AI Chatbot")
 
-# Function to call Google AI API
-def call_google_ai(prompt, model_name=choosen_model):
-    # Initialize the model
-    model = genai.GenerativeModel(model_name)  # Use a valid model name
-    # Generate content
-    response = model.generate_content(prompt)
-    # Strip leading newline if present
-    if response.text.startswith('\n'):
-        response.text = response.text[1:]
-    return response.text
+    # Create a session state variable to store the chat messages. This ensures that the
+    # messages persist across reruns.
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-#print(call_google_ai("Hello, how are you?"))  # Example prompt
-# streamlit app
-st.title("💬Google Gemini AI Chatbot")
-
-# Create a session state variable to store the chat messages. This ensures that the
-# messages persist across reruns.
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display the existing chat messages via `st.chat_message`.
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Display the existing chat messages via `st.chat_message`.
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 
-# Create a chat input field to allow the user to enter a message. This will display
-# automatically at the bottom of the page.
-if prompt := st.chat_input("What is up?"):
+    # Create a chat input field to allow the user to enter a message. This will display
+    # automatically at the bottom of the page.
+    if prompt := st.chat_input("What is up?"):
 
-    # Store and display the current prompt.
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+        # Store and display the current prompt.
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
-#generate a response from the AI model using the prompt.
-    response = call_google_ai(prompt)
+    #generate a response from the AI model using the prompt.
+        response = call_google_ai(prompt)
 
-    # Store and display the AI's response.
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    with st.chat_message("assistant"):
-        st.markdown(response)
+        # Store and display the AI's response.
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        with st.chat_message("assistant"):
+            st.markdown(response)
